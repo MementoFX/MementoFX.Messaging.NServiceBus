88c8ba32882c6e17e55cd669c781343264add850
Andrea Saltarello <andysal@gmail.com>
2017-03-10 18:23:04 +0100

    1.0.1-pre2

---
+++ src/Memento.Messaging.NServiceBus.Tests/NServiceBusEventDispatcherFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTestsEx;
+
+namespace Memento.Messaging.NServiceBus.Tests
+{
+    [TestClass]
+    public class NServiceBusEventDispatcherFixture
+    {
+        [TestMethod]
+        public void Ctor_should_throw_ArgumentNullException_on_null_bus_and_value_of_parameter_should_be_bus()
+        {
+            Executing.This(() => new NServiceBusEventDispatcher(null))
+                .Should()
+                .Throw<ArgumentNullException>()
+                .And
+                .ValueOf
+                .ParamName
+                .Should()
+                .Be
+                .EqualTo("bus");
+        }
+    }
+}
---
+++ src/Memento.Messaging.NServiceBus/NServiceBusEventDispatcher.cs
@@ -0,0 +1,30 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Messaging.NServiceBus
+{
+    public class NServiceBusEventDispatcher : IEventDispatcher
+    {
+        protected IBus Bus { get; private set; }
+
+        public NServiceBusEventDispatcher(IBus bus)
+        {
+            if (bus == null)
+                throw new ArgumentNullException(nameof(bus));
+
+            Bus = bus;
+        }
+
+        public void Dispatch<T>(T @event) where T : DomainEvent
+        {
+            if (@event == null)
+                throw new ArgumentNullException(nameof(@event));
+
+            Bus.Publish(@event);
+        }
+    }
+}
